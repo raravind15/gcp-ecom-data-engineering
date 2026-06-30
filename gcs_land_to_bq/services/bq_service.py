2850471bae67bf68a6a685620f0f9e84bc0cab11
raravind15 <raravind15@gmail.com>
2026-06-30 17:10:51 +0530

    Refactored land_to_bq-fixed bq_service.py:v10

--- gcs_land_to_bq/services/bq_service.py
+++ gcs_land_to_bq/services/bq_service.py
@@ -65,7 +65,29 @@ def is_file_already_processed(file_name):
 
     return False
 
-def mark_file_processing(file_name):
+# def mark_file_processing(file_name):
+
+#     table_id = (
+#         f"{bq_client.project}."
+#         f"{config.AUDIT_DATASET}."
+#         f"processed_files"
+#     )
+
+#     rows = [
+#         {
+#             "file_name": file_name,
+#             "status": "PROCESSING",
+#             "created_timestamp":
+#                 datetime.now(timezone.utc).isoformat()
+#         }
+#     ]
+
+#     result=bq_client.insert_rows_json(
+#         table_id,
+#         rows
+#     )
+
+def mark_file_success(file_name):
 
     table_id = (
         f"{bq_client.project}."
@@ -76,69 +98,50 @@ def mark_file_processing(file_name):
     rows = [
         {
             "file_name": file_name,
-            "status": "PROCESSING",
+            "status": "SUCCESS",
             "created_timestamp":
                 datetime.now(timezone.utc).isoformat()
         }
     ]
 
-    bq_client.insert_rows_json(
+    errors = bq_client.insert_rows_json(
         table_id,
         rows
     )
 
-def mark_file_success(file_name):
-
-    query = f"""
-    UPDATE `{bq_client.project}.{config.AUDIT_DATASET}.processed_files`
-    SET
-        status = 'SUCCESS'
-    WHERE file_name = @file_name
-    """
-
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter(
-                "file_name",
-                "STRING",
-                file_name
-            )
-        ]
-    )
-
-    bq_client.query(
-        query,
-        job_config=job_config
-    ).result()
+    if errors:
+        raise Exception(
+            f"Processed file insert failed: {errors}"
+        )
 
     logging.info(
         f"File marked as SUCCESS: {file_name}"
     )
 
-    def mark_file_failed(file_name):
+    # def mark_file_failed(file_name):
 
-        query = f"""
-        UPDATE `{bq_client.project}.{config.AUDIT_DATASET}.processed_files`
-        SET
-            status = 'FAILED'
-        WHERE file_name = @file_name
-        """
+    #     query = f"""
+    #     UPDATE `{bq_client.project}.{config.AUDIT_DATASET}.processed_files`
+    #     SET
+    #         status = 'FAILED'
+    #     WHERE file_name = @file_name
+    #     """
 
-        job_config = bigquery.QueryJobConfig(
-            query_parameters=[
-                bigquery.ScalarQueryParameter(
-                    "file_name",
-                    "STRING",
-                    file_name
-                )
-            ]
-        )
+    #     job_config = bigquery.QueryJobConfig(
+    #         query_parameters=[
+    #             bigquery.ScalarQueryParameter(
+    #                 "file_name",
+    #                 "STRING",
+    #                 file_name
+    #             )
+    #         ]
+    #     )
 
-        bq_client.query(
-            query,
-            job_config=job_config
-        ).result()
+    #     bq_client.query(
+    #         query,
+    #         job_config=job_config
+    #     ).result()
 
-        logging.info(
-            f"File marked as FAILED: {file_name}"
-        )
+    #     logging.info(
+    #         f"File marked as FAILED: {file_name}"
+    #     )
